c80431cb96ba9dd8c025b5a9110b80c9159fdde4
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2021-11-25 21:49:16 +0800

    video player

--- app/src/main/java/com/example/daybreak/MainActivity.java
+++ app/src/main/java/com/example/daybreak/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.daybreak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.scenery;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
     }
 }
