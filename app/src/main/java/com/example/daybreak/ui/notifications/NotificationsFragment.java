8fc26600ff7a9847fc72acae6be19948ba18d1f2
Matthew Chan <mchanjh1@gmail.com>
2022-01-03 22:34:54 +0800

    missing logic, will implement

--- app/src/main/java/com/example/daybreak/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/daybreak/ui/notifications/NotificationsFragment.java
@@ -28,13 +28,7 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 
