ee8fea1c41287e3047bd8331a965fbd62812499a
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2022-01-07 21:26:03 +0800

    Shared preference feature to save user login details

--- app/src/main/java/com/example/daybreak/LoginActivity.java
+++ app/src/main/java/com/example/daybreak/LoginActivity.java
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -25,10 +27,14 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
+    private Boolean saveLogin;
+    private CheckBox save_login_checkbox;
     private TextInputLayout email_text_input;
     private TextInputEditText email_edit_text;
     private TextInputLayout passwordTextInput;
     private TextInputEditText passwordEditText;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         passwordEditText = findViewById(R.id.password_edit_text);
         email_text_input = findViewById(R.id.email_text_input);
         email_edit_text = findViewById(R.id.email_edit_text);
+        save_login_checkbox = (CheckBox) findViewById(R.id.save_login_checkbox);
+
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            email_edit_text.setText(loginPreferences.getString("username", ""));
+            passwordEditText.setText(loginPreferences.getString("password", ""));
+            save_login_checkbox.setChecked(true);
+        }
 
         // KEYSTROKE LISTENER
         // Clear the error once more than 8 characters are typed.
@@ -70,6 +87,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent i;
         Bundle animationBundle = ActivityOptions.makeCustomAnimation(this, R.animator.slide_in_left,
                 R.animator.slide_out_right).toBundle();
+
 
         switch (v.getId()) {
             case R.id.back_button:
@@ -108,6 +126,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+                                    if (save_login_checkbox.isChecked()) {
+                                        loginPrefsEditor.putBoolean("saveLogin", true);
+                                        loginPrefsEditor.putString("username", email_text_input_string);
+                                        loginPrefsEditor.putString("password", password_text_input_string);
+                                        loginPrefsEditor.commit();
+                                    } else {
+                                        loginPrefsEditor.clear();
+                                        loginPrefsEditor.commit();
+                                    }
+
                                     passwordTextInput.setErrorEnabled(false); // Clear the error
                                     Intent i = new Intent(getApplicationContext(), MainActivity.class);
                                     startActivity(i);
