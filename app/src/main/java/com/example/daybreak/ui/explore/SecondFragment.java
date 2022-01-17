a36812486dd1efc114f3fd84f93c175dc5193a7c
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2022-01-17 09:45:22 +0800

    Grid layout

--- app/src/main/java/com/example/daybreak/ui/explore/SecondFragment.java
+++ app/src/main/java/com/example/daybreak/ui/explore/SecondFragment.java
@@ -1,14 +1,21 @@
 package com.example.daybreak.ui.explore;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.daybreak.InnerExploreActivity;
 import com.example.daybreak.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +28,12 @@ public class SecondFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private RecyclerView singlePracticeRecyclerView;
+    private RecyclerView meditationSeriesRecyclerView;
+    private RecyclerView forYouSeriesRecyclerView;
+    private ArrayList<RecyclerExploreCardItem> singlePracticeItems = new ArrayList<>();
+    private ArrayList<RecyclerExploreCardItem> meditationSeriesItems = new ArrayList<>();
+    private ArrayList<RecyclerExploreLongCardItem> forYouSeriesItems = new ArrayList<>();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +74,57 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        bindPodCastData();
+        bindSinglePracticeItems(view);
+
+        return view;
+    }
+
+    private void bindPodCastData() {
+        // SINGLE SERIES
+        singlePracticeItems.add(new RecyclerExploreCardItem("Cloud", "10 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_4)));
+        singlePracticeItems.add(new RecyclerExploreCardItem("Sedentary", "5-15 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_2)));
+        singlePracticeItems.add(new RecyclerExploreCardItem("Morning Planning", "5-10 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_3)));
+        singlePracticeItems.add(new RecyclerExploreCardItem("Insomnia", "5-15 Min • Singles", getActivity().getDrawable(R.drawable.background_image_1)));
+        singlePracticeItems.add(new RecyclerExploreCardItem("Work Burnout", "5-10 Min • Singles", getActivity().getDrawable(R.drawable.background_image_2)));
+        singlePracticeItems.add(new RecyclerExploreCardItem("Falling Asleep", "5 Min • Singles", getActivity().getDrawable(R.drawable.background_image_3)));
+
+        // MEDITATION SERIES
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Dreamland", "10 Min • Singles", getActivity().getDrawable(R.drawable.background_image_3)));
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Fantasyland", "5-10 Min • Singles", getActivity().getDrawable(R.drawable.background_image_2)));
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Morning Exercise", "5 Min • Singles", getActivity().getDrawable(R.drawable.background_image_1)));
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Healthy Eating", "10 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_3)));
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Happiness", "5-15 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_2)));
+        meditationSeriesItems.add(new RecyclerExploreCardItem("Coping With Stress", "5-15 Min • Singles", getActivity().getDrawable(R.drawable.calm_background_4)));
+
+        // SELECTED MIX
+
+        // SELECTED STORY
+
+
+        // FOR YOU
+        forYouSeriesItems.add(new RecyclerExploreLongCardItem("Steps", "Slow down your pace, feel the wonderful coordination of your body, and meet your own true self.", getActivity().getDrawable(R.drawable.calm_background_4), new String[]{"Meditation", "Melody"}));
+        forYouSeriesItems.add(new RecyclerExploreLongCardItem("Street", "I know this one the best. I think of the streets in Japan, which are quiet and full of lovely shops. I want this alarm!", getActivity().getDrawable(R.drawable.calm_background_1), new String[]{"Nature", "Sleep"}));
+        forYouSeriesItems.add(new RecyclerExploreLongCardItem("Lighthouse", "As I inhale the impalpable breeze that set in upon me, the ocean mysterious rolls toward me closer and closer.", getActivity().getDrawable(R.drawable.calm_background_3), new String[]{"Nature", "Meditation"}));
+        forYouSeriesItems.add(new RecyclerExploreLongCardItem("Library", "Close the book, still feeling what it was like to dwell in that light.", getActivity().getDrawable(R.drawable.calm_background_2), new String[]{"Performance", "Stress"}));
+    }
+
+    public void bindSinglePracticeItems(View view) {
+        // SINGLE SERIES
+        singlePracticeRecyclerView = view.findViewById(R.id.selected_podcasts);
+        // Set Layout Manager to RecyclerView
+        singlePracticeRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        //Create adapter
+        RecyclerExploreCardAdapter myRecyclerViewAdapter = new RecyclerExploreCardAdapter(singlePracticeItems, new RecyclerExploreCardAdapter.MyRecyclerViewItemClickListener() {
+            //Handling clicks
+            @Override
+            public void onItemClicked(RecyclerExploreCardItem country) {
+                Intent intent = new Intent(getActivity(), InnerExploreActivity.class);
+
+                startActivity(intent);
+            }
+        });
+        singlePracticeRecyclerView.setAdapter(myRecyclerViewAdapter);
     }
 }
